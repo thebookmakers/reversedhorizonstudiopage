4283de28a5145d4ad30573be12800741c60be615
thebookmakers <reversed.horizon.studio@gmail.com>
2025-06-08 21:56:32 +0700

    init

---
+++ js/jsonEditor.js
@@ -0,0 +1,48 @@
+﻿let editor;
+let dotNetRef;
+
+window.jsonEditorInterop = {
+    init: function (elementId, initialValue, dotNetHelper) {
+        editor = ace.edit(elementId);
+        editor.setTheme("ace/theme/github");
+        editor.session.setMode("ace/mode/json");
+        editor.setValue(initialValue, -1);
+        editor.session.setUseWrapMode(true);
+        editor.setOption("wrap", true);
+        editor.setShowPrintMargin(false);
+
+        dotNetRef = dotNetHelper;
+        dotNetHelper.invokeMethodAsync("OnEditorReady");
+        editor.session.selection.on('changeCursor', reportCursorInfo);
+        editor.session.selection.on('changeSelection', reportCursorInfo);
+    },
+    getValue: function () {
+        return editor.getValue();
+    },
+    setValue: function (json) {
+        editor.setValue(json, -1);
+    },
+    compressJson: function (json) {
+        return LZString.compressToEncodedURIComponent(json);
+    },
+    decompressJson: function (encoded) {
+        return LZString.decompressFromEncodedURIComponent(encoded);
+    },
+    getHashParams: function () {
+        const hash = window.location.hash.substring(1);
+        return Object.fromEntries(new URLSearchParams(hash));
+    },
+    getCompressedSize: function (json) {
+        const compressed = LZString.compressToEncodedURIComponent(json);
+        const byteLength = new TextEncoder().encode(compressed).length;
+        return { compressed, size: byteLength };
+    },
+};
+
+function reportCursorInfo() {
+    if (!dotNetRef) return;
+    const pos = editor.getCursorPosition();
+    const range = editor.getSelectionRange();
+    const selectedText = editor.session.getTextRange(range);
+    dotNetRef.invokeMethodAsync("UpdateEditorStatus", pos.row + 1, pos.column + 1, selectedText.length);
+}
